5d08991289be3eff60cac936e3418edf58c5f7b0
VivaVlad <vlad.ivashenko.98@mail.ru>
2017-05-16 14:58:58 +0300

    Added new data accessing objects

---
+++ src/main/java/com/poligram/dao/UserDao.java
@@ -0,0 +1,21 @@
+package com.poligram.dao;
+
+import java.util.List;
+
+import com.poligram.model.User;
+
+
+public interface UserDao {
+
+	User findById(int id);
+	
+	User findBySSO(String sso);
+	
+	void save(User user);
+	
+	void deleteBySSO(String sso);
+	
+	List<User> findAllUsers();
+
+}
+
