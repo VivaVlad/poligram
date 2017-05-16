4b9dc3eb102acfc44195234370e66e9ebf3c91b6
VivaVlad <vlad.ivashenko.98@mail.ru>
2017-05-16 14:49:12 +0300

    Updated application configuration

--- src/main/java/com/poligram/configuration/AppInitializer.java
+++ src/main/java/com/poligram/configuration/AppInitializer.java
@@ -11,34 +11,34 @@ public class AppInitializer extends AbstractAnnotationConfigDispatcherServletIni
 	protected Class<?>[] getRootConfigClasses() {
 		return new Class[] { AppConfiguration.class };
 	}
- 
+
 	@Override
 	protected Class<?>[] getServletConfigClasses() {
 		return null;
 	}
- 
+
 	@Override
 	protected String[] getServletMappings() {
 		return new String[] { "/" };
 	}
 
-    @Override
+	@Override
 	protected void customizeRegistration(ServletRegistration.Dynamic registration) {
-    	registration.setMultipartConfig(getMultipartConfigElement());
+		registration.setMultipartConfig(getMultipartConfigElement());
 	}
 
-    private MultipartConfigElement getMultipartConfigElement(){
+	private MultipartConfigElement getMultipartConfigElement(){
 		MultipartConfigElement multipartConfigElement = new MultipartConfigElement(LOCATION, MAX_FILE_SIZE, MAX_REQUEST_SIZE, FILE_SIZE_THRESHOLD);
 		return multipartConfigElement;
 	}
-    
-    /*Set these variables for your project needs*/ 
-    
+
+    /*Set these variables for your project needs*/
+
 	private static final String LOCATION = "/home/stazhor/git/storage";
 
 	private static final long MAX_FILE_SIZE = 1024 * 1024 * 25;//25MB
-	
+
 	private static final long MAX_REQUEST_SIZE = 1024 * 1024 * 30;//30MB
 
-    private static final int FILE_SIZE_THRESHOLD = 0;
+	private static final int FILE_SIZE_THRESHOLD = 0;
 }
